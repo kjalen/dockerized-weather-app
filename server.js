dc96dd63f22e08ae10460abd0305102469a6283a
Kjalen Hansen <kjalenhansen@gmail.com>
2021-07-01 17:37:57 -0700

    externalized weatherapi class

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
-'use strict';
+const weatherApi = require('./www/weatherapi')
 require('dotenv').config();
-const fetch = require("isomorphic-unfetch")
-const { Headers } = require('node-fetch');
+
+'use strict';
+
+
 const express = require('express');
-const querystring = require("querystring");
 
-class WeatherAPI {
-    constructor(config) {
-        this.api_key = config.api_key
-        this.basePath = "https://api.openweathermap.org/data/2.5"
-    }
-    request(endpoint = "", options = {}) {
-        let url = this.basePath + endpoint
-        let headers = {
-            'x-api-key': this.api_key
-        };
-
-        return fetch(url, {headers: headers}).then(r => {
-            console.log(r.headers);
-            console.log(url);
-            if (r.ok) {
-                return r.json()
-            }
-            throw new Error(console.log(r.Error))
-        })
-
-    }
-    getWeather(options) {
-        console.log('options: ' + options);
-        let qs = options ? "?" + querystring.stringify(options) : ""
-        console.log('qs: ' + qs);
-        let url = "/find" + qs
-        let config = {
-            method: 'GET'
-        }
-        return this.request(url, config)
-    }
-}
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -44,7 +13,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
-const api = new WeatherAPI({api_key: process.env.API_KEY})
+const api = new weatherApi({api_key: process.env.API_KEY})
 
 
 app.listen(PORT, HOST);
---
+++ www/weatherapi.js
@@ -0,0 +1,35 @@
+
+const fetch = require("isomorphic-unfetch")
+const querystring = require("querystring");
+class WeatherAPI {
+    constructor(config) {
+        this.api_key = config.api_key
+        this.basePath = "https://api.openweathermap.org/data/2.5"
+    }
+    request(endpoint = "", options = {}) {
+        let url = this.basePath + endpoint
+        let headers = {
+            'x-api-key': this.api_key
+        };
+        let method = options.method
+
+        return fetch(url, {method: method, headers: headers}).then(r => {
+            if (r.ok) {
+                return r.json()
+            }
+            throw new Error(console.log(r.statusText))
+        })
+
+    }
+    getWeather(options) {
+        console.log('options: ' + options);
+        let qs = options ? "?" + querystring.stringify(options) : ""
+        let url = "/find" + qs
+        let config = {
+            method: 'GET'
+        }
+        return this.request(url, config)
+    }
+}
+
+module.exports = WeatherAPI;
