128df6148ac4c6394c34fe4df605b019f9267b50
Kjalen Hansen <kjalenhansen@gmail.com>
2021-07-11 13:59:36 -0700

    cleaning up

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,10 +2,6 @@ const WeatherApi = require('../services/WeatherApi.service')
 require('dotenv').config();
 
 let api = new WeatherApi({api_key: process.env.API_KEY})
-//const api = new weatherApi({api_key: process.env.API_KEY})
 exports.index = function(req, res) {
     api.getWeatherByCity({q: req.params.city, units: 'metric'}).then(data => res.send(data));
 };
-
-// see if its working
-//api.getWeatherByCity({q: 'Victoria', units: 'metric'}).then(data => console.log(data))
